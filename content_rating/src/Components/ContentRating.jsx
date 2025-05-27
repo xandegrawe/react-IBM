b95d10d8dbdfafd27041949d8fcc8fd66b60e0c7
xandegrawe <xandegrawe@gmail.com>
2025-05-26 20:31:01 -0400

    add-content

---
+++ content_rating/src/Components/ContentRating.jsx
@@ -0,0 +1,18 @@
+
+import React, { Component } from 'react';
+import './ContentRating.css';
+
+class ContentRating extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+     <>
+     <h1>Text Content Rating</h1>
+     </>
+    );
+  }
+}
+
+export default ContentRating;
